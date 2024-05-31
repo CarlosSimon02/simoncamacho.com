28ca2e6abff65990d400fa537ffa47b19623680e
CarlosSimon02 <caloycamacho02@gmail.com>
2024-05-31 17:32:08 +0800

    feat(config): add i18n configuration and Tailwind CSS integration for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "fil"]
+  },
   integrations: [tailwind()]
 });
