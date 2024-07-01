23b27739076c397b99bc2c81b15471a4e407f2ef
CarlosSimon02 <caloycamacho02@gmail.com>
2024-07-01 14:20:37 +0800

    update: update image path

--- src/i18n/languages.ts
+++ src/i18n/languages.ts
@@ -1,6 +1,6 @@
 export const allLanguages = {
   en: { name: "English", og: "en_US" },
-  fil: { name: "Filipino", og: "fil_US" }
+  fil: { name: "Filipino", og: "fil_PH" }
 } as const;
 
 export type LanguageKeys = keyof typeof allLanguages;
